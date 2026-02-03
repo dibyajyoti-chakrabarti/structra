414f9a69b82168e15165527d85fb8b5b51d3a9ac
Dibyajyoti Chakrabarti <dibyajyoti.chakrabarti.122@gmail.com>
2026-02-04 00:18:43 +0530

    Added Questionare page and questionare mechanism

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -17,3 +17,10 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             full_name=validated_data.get('full_name', '')
         )
         return user
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        # FIXED: Changed 'id' to 'user_id' to match your model
+        fields = ('user_id', 'full_name', 'email', 'user_role', 'org_name', 'org_loc', 'is_new')
+        read_only_fields = ('user_id', 'email')
