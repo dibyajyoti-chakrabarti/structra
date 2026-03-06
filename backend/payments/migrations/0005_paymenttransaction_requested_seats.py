6ce39de6a974e1839b32c322d4dd3f531094d870
sarth-shah20 <sarthshah333@gmail.com>
2026-03-06 23:42:16 +0530

    remove stray character causing NameError

--- backend/payments/migrations/0005_paymenttransaction_requested_seats.py
+++ backend/payments/migrations/0005_paymenttransaction_requested_seats.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("payments", "0004_rename_payment_webh_event_t_6f57ab_idx_pay_wh_event_type_idx_and_more"),
+        ("payments", "0003_webhookeventlog"),
     ]
 
     operations = [
